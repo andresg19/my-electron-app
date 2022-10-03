24b4eb6f39764ffe5071aa38beeb52ffb7ec40c4
andresg19 <andygermain17@gmail.com>
2022-10-03 18:49:51 -0300

    Scripts de precarga

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const { ipcMain } = require('electron')
+const path = require('path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-  })
-
-  win.loadFile('index.html')
-}
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  ipcMain.handle('ping', () => 'pong')
+  win.loadFile('index.html');
+};
 
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
-
-
-  app.whenReady().then(() => {
-    createWindow()
-  
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-  })
-
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
